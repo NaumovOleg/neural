eabbfa209ce18253d546df2713d46ab790b7a5c0
Oleg <keeperoleg26@gmail.com>
2025-03-01 16:04:15 +0200

    wip

--- src/keras_lessons/numbers_nw.py
+++ src/keras_lessons/numbers_nw.py
@@ -3,12 +3,17 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 mnist = keras.datasets.mnist
 Dense = keras.layers.Dense
 Flatten = keras.layers.Flatten
 Sequential = keras.models.Sequential
 Input = keras.layers.Input
+Dropout = keras.layers.Dropout
+BatchNormalization = keras.layers.BatchNormalization
+
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = np.array(x_train)
@@ -30,29 +35,39 @@ model = Sequential()
 model.add(Input((28, 28, 1)))
 model.add(Flatten())
 model.add(Dense(100, activation="relu"))
+# model.add(Dropout(0.2))
+model.add(BatchNormalization())
 model.add(Dense(10, activation="softmax"))
 
 y_train_cat = keras.utils.to_categorical(y_train, 10)
 y_test_cat = keras.utils.to_categorical(y_test, 10)
 
-model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
-model.fit(
-    x_train_scaled,
-    y_train_cat,
-    batch_size=32,
+optimizer = keras.optimizers.Adam(learning_rate=0.001)
+model.compile(
+    optimizer=optimizer, loss="categorical_crossentropy", metrics=["accuracy"]
+)
+
+x_train, x_val, y_train, y_val = train_test_split(
+    x_train_scaled, y_train_cat, test_size=0.2
+)
+
+hist = model.fit(
+    x_train,
+    y_train,
+    batch_size=30,
     epochs=5,
     verbose=1,
-    validation_split=0.2,
+    validation_data=(x_val, y_val),
 )
 
 model.evaluate(x_test_scaled, y_test_cat)
 
 predicted = model.predict(x_test_scaled)
 predicted = np.argmax(predicted, axis=1)
-print(predicted, end="\n")
-
 
 not_valid_mask = predicted == y_test
 not_valid = y_test[~(predicted == y_test)]
 
-print(not_valid, end="\n")
+plt.plot(hist.history["loss"])
+plt.plot(hist.history["val_loss"])
+plt.show()
