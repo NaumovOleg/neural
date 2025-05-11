183fc3b5b89dd172dda2eb44b56f67714acb18bd
Oleg <keeperoleg26@gmail.com>
2025-05-11 22:06:23 +0300

    wip

--- src/object/custom.py
+++ src/object/custom.py
@@ -4,42 +4,28 @@ from prepare_dataset import VOCDataset
 import matplotlib.pyplot as plt
 from image_actions import show_image_with_boxes
 import numpy as np
-from custom_losses import iou_loss, giou_loss_batch
-
-tf.experimental.numpy.experimental_enable_numpy_behavior()
+from custom_losses import giou_loss_batch
 
 Input = keras.layers.Input
 Conv2D = keras.layers.Conv2D
-MaxPooling2D = keras.layers.MaxPooling2D
-GlobalAveragePooling2D = keras.layers.GlobalAveragePooling2D
 Reshape = keras.layers.Reshape
 Dense = keras.layers.Dense
 Flatten = keras.layers.Flatten
 Concatenate = keras.layers.Concatenate
 Activation = keras.layers.Activation
 Model = keras.models.Model
-Huber = keras.losses.Huber
-Lambda = keras.layers.Lambda
-MaxPooling2D = keras.layers.MaxPooling2D
-Huber = keras.losses.Huber
-CategoricalAccuracy = keras.metrics.CategoricalAccuracy
+CategoricalCrossentropy = keras.losses.CategoricalCrossentropy
 
 
 DATASET_PATH = "data_sets/object-detecton.v2i.voc/train"
 MAX_OBJECTS = 2
 CLASSES = 2
 dataset = VOCDataset(DATASET_PATH)
-
 x_train, y_boxes, y_labels = dataset.preprocess_dataset(dataset)
-
 y_labels = tf.one_hot(y_labels, depth=2).numpy()
-
-
 y_train = np.array(
     [[np.concatenate([box, label], axis=1)] for box, label in zip(y_boxes, y_labels)]
 ).reshape((-1, 2, 6))
-
-print(y_train.shape)
 
 
 inputs = Input(shape=(224, 224, 3))
@@ -48,24 +34,22 @@ x = Conv2D(128, 3, strides=2, padding="same", activation="relu")(x)
 x = Conv2D(256, 3, strides=2, padding="same", activation="relu")(x)
 x = Flatten()(x)
 x = Dense(512, activation="relu")(x)
-# x = Dense(256, activation="relu")(x)
 x = Dense(12)(x)
 x = Reshape((2, 6))(x)
 bbox_sigmoid = Activation("sigmoid")(x[..., :4])
 class_predictions = x[..., 4:]
 outputs = Concatenate(axis=-1)([bbox_sigmoid, class_predictions])
 
-
 model = Model(inputs=inputs, outputs=outputs)
-huber_loss = Huber()
+cross_entropy = CategoricalCrossentropy(from_logits=True)
 
 
 @tf.function
-def enforce_box_order(boxes):
-    x1 = tf.minimum(boxes[..., 0], boxes[..., 2])
-    y1 = tf.minimum(boxes[..., 1], boxes[..., 3])
-    x2 = tf.maximum(boxes[..., 0], boxes[..., 2])
-    y2 = tf.maximum(boxes[..., 1], boxes[..., 3])
+def enforce_box_order(boxvals):
+    x1 = tf.minimum(boxvals[..., 0], boxvals[..., 2])
+    y1 = tf.minimum(boxvals[..., 1], boxvals[..., 3])
+    x2 = tf.maximum(boxvals[..., 0], boxvals[..., 2])
+    y2 = tf.maximum(boxvals[..., 1], boxvals[..., 3])
     return tf.stack([x1, y1, x2, y2], axis=-1)
 
 
@@ -79,21 +63,15 @@ def custom_loss(y_true, y_pred):
 
     result = giou_loss_batch(true_boxes, pred_boxes)
     iou_losses = tf.reduce_mean(result)
-    tf.print("===============", iou_losses)
 
-    # box_loss = huber_loss(true_boxes, pred_boxes)
-    class_loss = tf.keras.losses.categorical_crossentropy(
-        true_classes, pred_classes, from_logits=True
-    )
+    class_loss = cross_entropy(true_classes, pred_classes)
 
-    return class_loss + 10.0 * iou_losses
+    return class_loss + 9.1 * iou_losses
 
 
-model.compile(
-    optimizer="adam", loss=custom_loss, metrics=[CategoricalAccuracy(name="cls_acc")]
-)
+model.compile(optimizer="adam", loss=custom_loss, metrics=["accuracy"])
 
-history = model.fit(x_train, y_train, epochs=10)
+history = model.fit(x_train, y_train, epochs=7)
 to_predict = x_train[0:4]
 predicted = model.predict(to_predict)
 
--- src/object/custom_losses/iou.py
+++ src/object/custom_losses/iou.py
@@ -1,51 +1,17 @@
 import tensorflow as tf
 
 
-def iou_loss(bboxes):
-    y_true, y_pred = bboxes
-    tf.print("==========", y_true)
-    return 1
-    # x1 = tf.maximum(y_true[..., 0], y_pred[..., 0])
-    # y1 = tf.maximum(y_true[..., 1], y_pred[..., 1])
-    # x2 = tf.minimum(y_true[..., 2], y_pred[..., 2])
-    # y2 = tf.minimum(y_true[..., 3], y_pred[..., 3])
-
-    # inter_area = tf.maximum(x2 - x1, 0) * tf.maximum(y2 - y1, 0)
-
-    # # Union
-    # area_true = (y_true[..., 2] - y_true[..., 0]) * (y_true[..., 3] - y_true[..., 1])
-    # area_pred = (y_pred[..., 2] - y_pred[..., 0]) * (y_pred[..., 3] - y_pred[..., 1])
-    # union = area_true + area_pred - inter_area
-
-    # iou = inter_area / (union + 1e-7)
-    # xc1 = tf.minimum(y_true[..., 0], y_pred[..., 0])
-    # yc1 = tf.minimum(y_true[..., 1], y_pred[..., 1])
-    # xc2 = tf.maximum(y_true[..., 2], y_pred[..., 2])
-    # yc2 = tf.maximum(y_true[..., 3], y_pred[..., 3])
-
-    # c_area = (xc2 - xc1) * (yc2 - yc1)
-
-    # giou = iou - (c_area - union) / (c_area + 1e-7)
-    # return 1 - giou
-
-
+@tf.function
 def giou_loss_single(y_true, y_pred):
-    # Intersection
     x1 = tf.maximum(y_true[0], y_pred[0])
     y1 = tf.maximum(y_true[1], y_pred[1])
     x2 = tf.minimum(y_true[2], y_pred[2])
     y2 = tf.minimum(y_true[3], y_pred[3])
-
     inter_area = tf.maximum(x2 - x1, 0.0) * tf.maximum(y2 - y1, 0.0)
-
-    # Union
     area_true = (y_true[2] - y_true[0]) * (y_true[3] - y_true[1])
     area_pred = (y_pred[2] - y_pred[0]) * (y_pred[3] - y_pred[1])
     union = area_true + area_pred - inter_area
-
     iou = inter_area / (union + 1e-7)
-
-    # Smallest enclosing box (for GIoU)
     xc1 = tf.minimum(y_true[0], y_pred[0])
     yc1 = tf.minimum(y_true[1], y_pred[1])
     xc2 = tf.maximum(y_true[2], y_pred[2])
@@ -57,21 +23,16 @@ def giou_loss_single(y_true, y_pred):
     return 1.0 - giou
 
 
-# 👇 Функция для всей выборки (batch)
+@tf.function
 def giou_loss_batch(y_true, y_pred):
-    # y_true, y_pred: (batch_size, num_boxes, 4)
 
     def loss_per_sample(inputs):
-        true_sample, pred_sample = inputs  # shape: (num_boxes, 4)
+        true_sample, pred_sample = inputs
         losses = tf.map_fn(
             lambda x: giou_loss_single(x[0], x[1]),
             (true_sample, pred_sample),
-            fn_output_signature=tf.float32,  # Указываем тип выходных данных
+            fn_output_signature=tf.float32,
         )
         return tf.reduce_mean(losses)
 
-    return tf.map_fn(
-        loss_per_sample,
-        (y_true, y_pred),
-        fn_output_signature=tf.float32,  # Указываем тип выходных данных для batch
-    )
+    return tf.map_fn(loss_per_sample, (y_true, y_pred), fn_output_signature=tf.float32)
