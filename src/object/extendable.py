9ccf3c53739feade7a3efe5fc1355ea3f309bbc4
Oleg <keeperoleg26@gmail.com>
2025-05-13 16:02:31 +0300

    wip

--- src/object/extendable.py
+++ src/object/extendable.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from image_actions import show_image_with_boxes
-
 from tensorflow.keras.layers import (
     Dense,
     Conv2D,
@@ -9,7 +8,7 @@ from tensorflow.keras.layers import (
     Activation,
     Lambda,
     Concatenate,
-    BatchNormalization,
+    Dropout,
 )
 from tensorflow.keras.models import Model
 from tensorflow.keras.losses import CategoricalCrossentropy
@@ -44,14 +43,13 @@ class ObjectDetector(Model):
         self.convv3 = Conv2D(
             256, 3, strides=2, padding="same", activation="relu", name="ConvV3"
         )
-        self.normalisation = BatchNormalization(name="Normalisation")
         self.flatten = Flatten(name="Flatten")
         self.dense1 = Dense(512, activation="relu", name="Dense1")
         self.dense2 = Dense(12, name="Dense2")
         self.bbox_output = Lambda(self.calculate_boxes)
         self.outputs = Concatenate(axis=-1)
+        self.dropout = Dropout(0.2)
         self.bbox_sigmoid = Activation("sigmoid")
-
         self.cross_entropy = CategoricalCrossentropy(from_logits=True)
 
     def call(self, inputs):
@@ -61,26 +59,25 @@ class ObjectDetector(Model):
         x = self.convv3(x)
         flatten = self.flatten(x)
         x = self.dense1(flatten)
+        x = self.dropout(x)
         x = self.dense2(x)
         x = tf.reshape(x, (-1, 2, 6))
         bbox_sigmoid = self.bbox_sigmoid(x[..., :4])
         class_predictions = x[..., 4:]
-
         bbox_output = self.bbox_output(bbox_sigmoid)
         return tf.concat([bbox_output, class_predictions], axis=-1)
 
     @staticmethod
     def calculate_boxes(box):
+        """calculate_boxes"""
         cx = box[..., 0]
         cy = box[..., 1]
         w = tf.maximum(box[..., 2], 0.01)
         h = tf.maximum(box[..., 3], 0.01)
-
         x1 = cx - w / 2.0
         y1 = cy - h / 2.0
         x2 = cx + w / 2.0
         y2 = cy + h / 2.0
-
         return tf.stack([x1, y1, x2, y2], axis=-1)
 
 
@@ -97,20 +94,17 @@ def compute_loss(y_true, y_pred):
 
     result = giou_loss_batch(true_boxes, pred_boxes)
     iou_losses = tf.reduce_mean(result)
-
     class_loss = cross_entropy(true_classes, pred_classes)
 
-    return class_loss + 8.12 * iou_losses
+    return class_loss + 9.12 * iou_losses
 
 
 model = ObjectDetector()
 
-
 model.compile(optimizer="adam", loss=compute_loss, metrics=["accuracy"])
-history = model.fit(x_train, y_train, epochs=7)
-to_predict = x_train[0:4]
+history = model.fit(x_train, y_train, epochs=15)
+to_predict = x_train[3:12]
 predicted = model.predict(to_predict)
-
 
 for bboxes, img in zip(predicted, to_predict):
     boxes = bboxes[:, :4]
@@ -120,6 +114,7 @@ for bboxes, img in zip(predicted, to_predict):
 
 
 def show_accuracy_plot():
+    """show_accuracy_plot"""
     plt.figure(figsize=(14, 7))
     plt.plot(history.history["loss"], label="loss")
     plt.title("loss")
